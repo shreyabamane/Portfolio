4fbeb3ce10165789b53b6a42fff412a92e4f1fa4
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-05-25 17:06:56 +0530

    add light and dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -6,25 +6,29 @@ import { Contact } from "./pages/Contact/Contact";
 import { Education } from "./pages/Educations/Education";
 import { Projects } from "./pages/Projects/Projects";
 import { Techstack } from "./pages/Techstack/Techstack";
+import { useTheme } from "./context/ThemeContext";
 
 
 
 function App() {
+  const [theme] = useTheme();
   return (
     <>
-      <Layout />
-      <div className="container">
-        <About />
-        <Education />
-        <Techstack />
-        <Projects />
-        <Contact />
-      </div>
+      <div id={theme}>
+        <Layout />
+        <div className="container">
+          <About />
+          <Education />
+          <Techstack />
+          <Projects />
+          <Contact />
+        </div>
 
-      <div className="footer mb-3 ms-3">
-        <h4 className="text-center">
-          Made With ❤ Shreya &copy; 2024
-        </h4>
+        <div className="footer pb-3 ms-3">
+          <h4 className="text-center">
+            Made With ❤ Shreya &copy; 2024
+          </h4>
+        </div>
       </div>
 
       <ScrollToTop
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { useTheme } from '../../context/ThemeContext';
 import Typewriter from 'typewriter-effect';
+import { BsFillMoonStarsFill, BsFillSunFill } from 'react-icons/bs';
 
 import './Home.css';
 import Resume from '../../assets/docs/Shreya_Bamane-Resume.pdf'
 
 export function Home() {
+
+  const [theme, setTheme] = useTheme();
+
+  //handle theme
+  const handleTheme = () => {
+    setTheme((prevState) => (prevState === 'light' ? 'dark' : 'light'));
+  }
+
   return (
     <>
       <div className='container-fluid home-container' id='home'>
+        <div className='theme-btn' onClick={handleTheme}>
+          {theme === 'light' ? (<BsFillMoonStarsFill size={30}/>) : (<BsFillSunFill size={30}/>)}
+        </div>
         <div className='container home-content'>
           <h2>Hi 👋 I'm a </h2>
           <h1>
